12a982dd154f0f9c30d9c07e63567d329ae75217
rakeshsm119 <rakeshsm119@gmail.com>
2022-10-13 16:06:22 +0530

    First commit task Manager app

---
+++ controller/taskController.js
@@ -0,0 +1,14 @@
+const taskController = {
+
+    index: async(req,res) => {
+        res.render(`index`)
+    },
+    new: async(req,res) => {
+        res.render(`create`)
+    },
+    edit: async (req,res) => {
+        res.render(`update`)
+    }
+}
+
+module.exports = taskController
